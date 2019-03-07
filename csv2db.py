01c764f4fa05c9d4b220a803fdd14b00c0104d34
lifelonglearner127 <life.long.learner127@outlook.com>
2019-03-07 00:23:35 -0800

    parse the data

---
+++ csv2db.py
@@ -0,0 +1,58 @@
+from __future__ import print_function
+import os
+import time
+import argparse
+import pandas as pd
+from csvdb import CsvDB
+
+
+# Argument Parser
+ap = argparse.ArgumentParser()
+ap.add_argument('--input', required=True,
+                help='Path to the location of file or directory')
+ap.add_argument('--keepdb', action='store_true',
+                help='Keep the existing db if specified')
+ap.add_argument('--header', action='store_true',
+                help='First column is description')
+args = vars(ap.parse_args())
+
+db = CsvDB()
+
+# drop and create a new schema if keepdb flag not specified
+if not args['keepdb']:
+    db.drop_all()
+    db.create_all()
+
+start_time = time.time()
+
+# check if the input file is file or directory
+if os.path.isdir(args['input']):
+    print('Input is a directory...\nSearchiing files in this location')
+
+elif os.path.isfile(args['input']):
+    print('Input is a single file')
+
+    # read csv
+    header = 0 if args['header'] else None
+    df = pd.read_csv(
+        args['input'],
+        header=header,
+        names=[
+            'date', 'instrument_parameter', 'parameter_id',
+            'data_status', 'value', 'ignore'
+        ]
+    )
+
+    # parse column
+    df_instrument_parameter = df['instrument_parameter'].str.extract(
+        '(?P<instrument>\\w+:\\s*\\w+\\s*):(?P<parameter>\\s*\\w+)'
+    )
+    df = pd.concat([df, df_instrument_parameter], axis=1)
+    df = df.drop(['instrument_parameter'], axis=1)
+
+else:
+    print('Invalid input')
+
+
+end_time = time.time()
+print('Elapsed time: {}'.format(end_time - start_time))
