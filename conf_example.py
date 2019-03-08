bc275c31e54235d043a19b328bf3f6abc8231287
lifelonglearner127 <life.long.learner127@outlook.com>
2019-03-07 20:02:16 -0800

    use mysql connector

--- conf_example.py
+++ conf_example.py
@@ -1,1 +1,35 @@
-DB = 'postgresql://username:password@localhost/dbname'
+HOST = 'localhost'
+USER = 'name'
+PASSWD = 'password'
+DB_NAME = 'dbname'
+TABLES = {}
+TABLES['instruments'] = (
+    "CREATE TABLE `instruments` ("
+    "  `id` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `name` varchar(30) NOT NULL,"
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['parameters'] = (
+    "CREATE TABLE `parameters` ("
+    "  `id` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `name` varchar(30) NOT NULL,"
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['data'] = (
+    "CREATE TABLE `data` ("
+    "  `id` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `parameter_id` int(11) NOT NULL,"
+    "  `value` float(11) NOT NULL,"
+    "  `to_date` date NOT NULL,"
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['datastatus'] = (
+    "CREATE TABLE `datastatus` ("
+    "  `id` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `datum_id` int(11) NOT NULL,"
+    "  `status_id` int(11) NOT NULL,"
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")
--- csv2db.py
+++ csv2db.py
@@ -1,9 +1,31 @@
 from __future__ import print_function
 import os
 import time
+import conf
 import argparse
 import pandas as pd
-from csvdb import CsvDB
+import mysql.connector
+from mysql.connector import errorcode
+
+
+def create_database(cursor):
+    try:
+        cursor.execute(
+            "CREATE DATABASE IF NOT EXISTS {} DEFAULT CHARACTER SET 'utf8'"
+            .format(conf.DB_NAME)
+        )
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
+
+
+def drop_database(cursor):
+    try:
+        cursor.execute(
+            "DROP DATABASE IF EXISTS {}".format(conf.DB_NAME))
+    except mysql.connector.Error as err:
+        print("Failed creating database: {}".format(err))
+        exit(1)
 
 
 def parse_file(filename, header):
@@ -37,14 +59,48 @@ if __name__ == '__main__':
                     help='First column is description')
     args = vars(ap.parse_args())
 
-    db = CsvDB()
-
-    # drop and create a new schema if keepdb flag not specified
-    if not args['keepdb']:
-        db.drop_all()
-        db.create_all()
-
     start_time = time.time()
+
+    # connect mysql server
+    cnx = mysql.connector.connect(
+        host=conf.HOST,
+        user=conf.USER,
+        passwd=conf.PASSWD
+    )
+    cursor = cnx.cursor()
+
+    # drop the database
+    if not args['keepdb']:
+        drop_database(cursor)
+        print("Database {} dropped successfully.".format(conf.DB_NAME))
+
+    # use database; if not exists create databases
+    try:
+        cursor.execute("USE {}".format(conf.DB_NAME))
+    except mysql.connector.Error as err:
+        print("Database {} does not exists.".format(conf.DB_NAME))
+        if err.errno == errorcode.ER_BAD_DB_ERROR:
+            create_database(cursor)
+            print("Database {} created successfully.".format(conf.DB_NAME))
+            cnx.database = conf.DB_NAME
+        else:
+            print(err)
+            exit(1)
+
+    # create tables
+    for table_name in conf.TABLES:
+        table_description = conf.TABLES[table_name]
+        try:
+            print("Creating table {}: ".format(table_name), end='')
+            cursor.execute(table_description)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                print("already exists.")
+            else:
+                print(err.msg)
+        else:
+            print("OK")
+
     header = 0 if args['header'] else None
 
     # check if the input file is file or directory
@@ -54,8 +110,12 @@ if __name__ == '__main__':
     elif os.path.isfile(args['input']):
         print('Input is a single file')
         df = parse_file(args['input'], header)
+
     else:
         print('Invalid input')
+
+    cursor.close()
+    cnx.close()
 
     end_time = time.time()
     print('Elapsed time: {}'.format(end_time - start_time))
