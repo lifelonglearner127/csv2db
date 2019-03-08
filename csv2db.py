335e64c27ed8993b2969622f3598b59a3ddc4b63
lifelonglearner127 <life.long.learner127@outlook.com>
2019-03-07 17:56:34 -0800

    remove sqlalchemy due to its performance

--- csv2db.py
+++ csv2db.py
@@ -6,34 +6,8 @@ import pandas as pd
 from csvdb import CsvDB
 
 
-# Argument Parser
-ap = argparse.ArgumentParser()
-ap.add_argument('--input', required=True,
-                help='Path to the location of file or directory')
-ap.add_argument('--keepdb', action='store_true',
-                help='Keep the existing db if specified')
-ap.add_argument('--header', action='store_true',
-                help='First column is description')
-args = vars(ap.parse_args())
-
-db = CsvDB()
-
-# drop and create a new schema if keepdb flag not specified
-if not args['keepdb']:
-    db.drop_all()
-    db.create_all()
-
-start_time = time.time()
-
-# check if the input file is file or directory
-if os.path.isdir(args['input']):
-    print('Input is a directory...\nSearchiing files in this location')
-
-elif os.path.isfile(args['input']):
-    print('Input is a single file')
-
+def parse_file(filename, header):
     # read csv
-    header = 0 if args['header'] else None
     df = pd.read_csv(
         args['input'],
         header=header,
@@ -49,10 +23,39 @@ elif os.path.isfile(args['input']):
     )
     df = pd.concat([df, df_instrument_parameter], axis=1)
     df = df.drop(['instrument_parameter'], axis=1)
-
-else:
-    print('Invalid input')
+    return df
 
 
-end_time = time.time()
-print('Elapsed time: {}'.format(end_time - start_time))
+if __name__ == '__main__':
+    # Argument Parser
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--input', required=True,
+                    help='Path to the location of file or directory')
+    ap.add_argument('--keepdb', action='store_true',
+                    help='Keep the existing db if specified')
+    ap.add_argument('--header', action='store_true',
+                    help='First column is description')
+    args = vars(ap.parse_args())
+
+    db = CsvDB()
+
+    # drop and create a new schema if keepdb flag not specified
+    if not args['keepdb']:
+        db.drop_all()
+        db.create_all()
+
+    start_time = time.time()
+    header = 0 if args['header'] else None
+
+    # check if the input file is file or directory
+    if os.path.isdir(args['input']):
+        print('Input is a directory...\nSearchiing files in this location')
+
+    elif os.path.isfile(args['input']):
+        print('Input is a single file')
+        df = parse_file(args['input'], header)
+    else:
+        print('Invalid input')
+
+    end_time = time.time()
+    print('Elapsed time: {}'.format(end_time - start_time))
